7b726f2b3bbfe0d36d0390bea583f5a03d56bc9b
rynczakd <rynczak.damian@outlook.com>
2023-07-02 00:03:50 +0200

    ASR-proj: Added Linear layers, reducing mean options and reshaped output future maps for GRU network

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -4,8 +4,9 @@ import torch.nn.functional as F
 
 
 class FeatureExtractor(nn.Module):
-    def __init__(self):
+    def __init__(self, reduce_mean: bool = True):
         super(FeatureExtractor, self).__init__()
+        self.reduce_mean = reduce_mean
 
         # Define Convolutional and MaxPooling layers for Feature Extractor
         # CONV1
@@ -37,6 +38,15 @@ class FeatureExtractor(nn.Module):
                                stride=(1, 1),
                                padding=(1, 1))
 
+        if not self.reduce_mean:
+            # LINEAR LAYERS
+            self.fc1 = nn.Linear(in_features=6144, out_features=4096)
+            self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+            self.fc3 = nn.Linear(in_features=4096, out_features=128)
+
+            # DROPOUT LAYER
+            self.dropout = nn.Dropout(0.2)
+
     def forward(self, x):
         # Define forward method for Feature Extractor
         x = self.pool(F.relu(self.conv1(x)))
@@ -45,7 +55,19 @@ class FeatureExtractor(nn.Module):
         x = F.relu(self.conv4(x))
 
         # Perform averaging of feature maps over Channel dimension
-        x = torch.mean(x, dim=1, keepdim=True)
+        if self.reduce_mean:
+            x = torch.mean(x, dim=1, keepdim=True)
+            x = x.squeeze(1)  # Remove Channel dimension
+            x = x.permute(0, 2, 1).contiguous()  # Reshape feature maps into (B, W, H)
 
-        return x
+            return x  # Feature maps dimension - (B, W, H)
+
+        else:
+            x = x.permute(0, 3, 1, 2).contiguous()  # Reshape feature maps into (B, W, C, H)
+            x = x.view(x.size(0), x.size(1), -1)
+            x = self.dropout(F.relu(self.fc1(x)))
+            x = self.dropout(F.relu(self.fc2(x)))
+            x = F.relu(self.fc3(x))
+
+            return x  # Feature maps dimension - (B, W, H)
     
