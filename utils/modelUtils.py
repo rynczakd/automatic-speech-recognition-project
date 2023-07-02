8d57f244a1b9ada1bfdb0cdf3e853f1512d61694
rynczakd <rynczak.damian@outlook.com>
2023-07-02 03:10:22 +0200

    ASR-proj: Cleaned up modelUtils

--- utils/modelUtils.py
+++ utils/modelUtils.py
@@ -5,6 +5,7 @@ from model.feature_extractor import FeatureExtractor
 def mask_to_lengths(mask: torch.Tensor) -> torch.Tensor:
     # Convert padding mask to sample lengths
     mask_mean = torch.mean(mask, dim=2)
+
     return ((1 - mask_mean).sum(-1)).type(torch.int)
 
 
