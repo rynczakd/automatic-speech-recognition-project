e8f7db8fcf45f5cc4f3a09ff6a1c099c68d009fc
rynczakd <rynczak.damian@outlook.com>
2023-07-03 01:36:32 +0200

    ASR-proj: Moved Fully Connected layers into separated class and updated Feature Extractor hyperparameters

--- model/feature_extractor.py
+++ model/feature_extractor.py
@@ -3,8 +3,29 @@ from torch import nn
 import torch.nn.functional as F
 
 
+class FullyConnected(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super(FullyConnected, self).__init__()
+
+        self.fc1 = nn.Linear(in_features=6144, out_features=4096)
+        self.norm1 = nn.LayerNorm(4096)
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.norm2 = nn.LayerNorm(4096)
+        self.fc3 = nn.Linear(in_features=4096, out_features=512)
+        self.norm3 = nn.LayerNorm(512)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.dropout(F.gelu(self.norm1(self.fc1(x))))
+        x = self.dropout(F.gelu(self.norm2(self.fc2(x))))
+        x = F.gelu(self.norm3(self.fc3(x)))
+
+        return x
+
+
 class FeatureExtractor(nn.Module):
-    def __init__(self, reduce_mean: bool = True):
+    def __init__(self, reduce_mean: bool = False):
         super(FeatureExtractor, self).__init__()
         self.reduce_mean = reduce_mean
 
@@ -40,19 +61,14 @@ class FeatureExtractor(nn.Module):
 
         if not self.reduce_mean:
             # LINEAR LAYERS
-            self.fc1 = nn.Linear(in_features=6144, out_features=4096)
-            self.fc2 = nn.Linear(in_features=4096, out_features=4096)
-            self.fc3 = nn.Linear(in_features=4096, out_features=128)
-
-            # DROPOUT LAYER
-            self.dropout = nn.Dropout(0.2)
+            self.dense = FullyConnected(dropout=0.2)
 
     def forward(self, x):
         # Define forward method for Feature Extractor
-        x = self.pool(F.relu(self.conv1(x)))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
+        x = self.pool(F.gelu(self.conv1(x)))
+        x = F.gelu(self.conv2(x))
+        x = F.gelu(self.conv3(x))
+        x = F.gelu(self.conv4(x))
 
         # Perform averaging of feature maps over Channel dimension
         if self.reduce_mean:
@@ -63,8 +79,6 @@ class FeatureExtractor(nn.Module):
         else:
             x = x.permute(0, 3, 1, 2).contiguous()  # Reshape feature maps into (B, W, C, H)
             x = x.view(x.size(0), x.size(1), -1)
-            x = self.dropout(F.relu(self.fc1(x)))
-            x = self.dropout(F.relu(self.fc2(x)))
-            x = F.relu(self.fc3(x))
+            x = self.dense(x)
 
         return x  # Feature maps dimension - (B, W, H)
