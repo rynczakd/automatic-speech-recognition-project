e2decbfbf028967e33c793742be36067ad36cc5d
rynczakd <rynczak.damian@outlook.com>
2023-08-12 03:03:30 +0200

    ASR-project: Cleaned up running scripts

--- scripts/run_dataset_generation.py
+++ scripts/run_dataset_generation.py
@@ -5,6 +5,5 @@ if __name__ == '__main__':
     gin.parse_config_file("./automatic-speech-recognition-project/config/audio_preparation_config.gin")
     dataset_generator = AudioPreparation()
     print("CONFIG PARSED. STARTING GENERATION PROCESS...")
-
     dataset_generator.generate_database()
     print("PROCESS FINISHED WITHOUT ERRORS")
